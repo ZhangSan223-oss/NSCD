18bbdd7fb11603654f46cdce38cf9dd507b0cb92
ZhangSan223-oss <259783586+ZhangSan223-oss@users.noreply.github.com>
2026-02-07 10:24:14 +0800

    Update divide_data.py

--- divide_data.py
+++ divide_data.py
@@ -1,26 +1,22 @@
 import json
 import random
-#from bintrees import FastRBTree
+
 
 min_log = 15
 
 
 def divide_data():
-    '''
-    1. delete students who have fewer than min_log response logs
-    2. divide dataset into train_set, val_set and test_set (0.7:0.1:0.2)
-    :return:
-    '''
+
     with open('data/Assist/log_data.json', encoding='utf8') as i_f:
         stus = json.load(i_f)
-    # 1. delete students who have fewer than min_log response logs
+    
     stu_i = 0
     while stu_i < len(stus):
         if stus[stu_i]['log_num'] < min_log:
             del stus[stu_i]
             stu_i -= 1
         stu_i += 1
-    # 2. divide dataset into train_set, val_set and test_set
+    
     train_slice, train_set, val_set, test_set = [], [], [], []
     for stu in stus:
         user_id = stu['user_id']
@@ -43,7 +39,7 @@ def divide_data():
         train_slice.append(stu_train)
         val_set.append(stu_val)
         test_set.append(stu_test)
-        # shuffle logs in train_slice together, get train_set
+        
         for log in stu_train['logs']:
             train_set.append({'user_id': user_id, 'exer_id': log['exer_id'], 'score': log['score'],
                               'knowledge_code': log['knowledge_code']})
@@ -53,7 +49,7 @@ def divide_data():
     with open('data/Assist/train_set.json', 'w', encoding='utf8') as output_file:
         json.dump(train_set, output_file, indent=4, ensure_ascii=False)
     with open('data/Assist/val_set.json', 'w', encoding='utf8') as output_file:
-        json.dump(val_set, output_file, indent=4, ensure_ascii=False)    # 直接用test_set作为val_set
+        json.dump(val_set, output_file, indent=4, ensure_ascii=False)    
     with open('data/Assist/test_set.json', 'w', encoding='utf8') as output_file:
         json.dump(test_set, output_file, indent=4, ensure_ascii=False)
 
